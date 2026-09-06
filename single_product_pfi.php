4289e083580032129f6c19c1a52099e8cff4a5de
MD. SUJAN AHMED <csesujon155@gmail.com>
2026-09-06 15:09:21 +0600

    Image Changes

--- single_product_pfi.php
+++ single_product_pfi.php
@@ -33,14 +33,9 @@
         
         <!-- Project Images Header -->
         <div class="row mb-5">
-            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 mb-3">
-                <div class="single-project-image-box wow fadeInLeft" data-wow-delay="200ms" data-wow-duration="1200ms">
-                    <img src="images/projects/6.jpg" alt="Moon Star PFI Panel Project 1" class="img-fluid rounded shadow w-100" style="height: 350px; object-fit: cover;">
-                </div>
-            </div>
-            <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 mb-3">
+            <div class="col-xl-12 col-lg-6 col-md-6 col-sm-12 mb-3">
                 <div class="single-project-image-box wow fadeInRight" data-wow-delay="200ms" data-wow-duration="1200ms">
-                    <img src="images/projects/PFI/PFI-1.jpeg" alt="Moon Star PFI Panel Project 2" class="img-fluid rounded shadow w-100" style="height: 350px; object-fit: cover;">
+                    <img src="images/projects/PFI/PFI-1.jpg" alt="Moon Star PFI Panel Project 2" class="img-fluid rounded shadow w-100" style="height: 350px; object-fit: cover;">
                 </div>
             </div>
         </div>
